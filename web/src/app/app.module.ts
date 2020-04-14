58a3f9e270a0b0afb7837c26db04eb31f5d08eeb
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-04-14 16:57:21 +0300

    Web login form

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -7,16 +7,27 @@ import { AppComponent } from './app.component';
 import { AngularFireModule, } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { AuthComponent } from './auth/auth.component';
+import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
