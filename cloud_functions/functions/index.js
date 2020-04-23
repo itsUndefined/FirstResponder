154ec49ce97805e49615f30daf9008af88d6fcd0
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-04-23 19:37:50 +0300

    alert function done

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -3,15 +3,41 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const db = admin.firestore();
 
-exports.sendNotification = functions.firestore.document('alerts/{alertsId}').onCreate((change, context) => {
+const calculate = require('geofire');
+
+exports.alertUsers = functions.region('europe-west1').firestore.document('alerts/{alertsId}').onCreate((change, context) => {
     let payload = {
-      notification: {
-          title: 'cloud function',
-          message: 'cloud function works',
-      }
+        data: {
+            title: 'Alert',
+            message: 'Cloud function works'
+        }
     };
-    return admin.messaging().sendToDevice(
-        'fX9ekfYDKQg:APA91bHi4Msr8C4FOWcSu-pZ6EOXzPvU0M34H-SxhPJV93pslH1UVTFRftFH3AKYz-xeZqc4KRMRmN2WFgBXbD30062y5I4OZwk7hS10arI-2V9poUtyadUfKEzz4xD5Lg3ocGkYiaeU',
-        payload);
+    let counter = 0;
+    let mainLocation = [change.data()['location'].latitude, change.data()['location'].longitude];
+    let usersRef = db.collection('users');
+    let respondersList = usersRef.get()
+        // eslint-disable-next-line promise/always-return
+        .then(snapshot => {
+            let list = [];
+            snapshot.forEach((doc) => {
+                let userLocation = [doc.data()['location'].latitude, doc.data()['location'].longitude];
+                let distance = calculate.GeoFire.distance(mainLocation, userLocation);
+                if (distance <= change.data()['maxDistance']) {
+                    admin.messaging().sendToDevice(doc.data()['token'], payload)
+                        .then((res) => {
+                            return console.log('Successfully sent message:', res);
+                        })
+                        .catch((err) => {
+                            return console.log('Error sending message:', err);
+                        });
+                    counter++;
+                }
+                return counter > change.data()['maxUsers'];
+            });
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
 });
